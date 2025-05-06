b4ec56f195b999d68c22a562dee2a7ecb06fc12b
SantiagoMurillas <smurillas9@icloud.com>
2025-05-06 15:06:51 -0500

    Agregado Archivos Model Par-Impar

---
+++ Model/Model Par-Impar.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Juego.h"
+
+class ParImpar : public Juego {
+public:
+    void mostrarReglas() const override;
+    bool jugar(int apuesta) override;
+};
+//
+// Created by Santiago Murillas on 5/6/25.
+//
+
+#ifndef MODEL_PAR_IMPAR_H
+#define MODEL_PAR_IMPAR_H
+
+#endif //MODEL_PAR_IMPAR_H
