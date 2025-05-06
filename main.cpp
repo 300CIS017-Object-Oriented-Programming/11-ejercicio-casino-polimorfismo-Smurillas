23f6e670f23e2111c694fa29b01881d638478f32
SantiagoMurillas <smurillas9@icloud.com>
2025-05-06 15:47:02 -0500

    .

--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 #include "View/View.h"
 #include <iostream>
-#include "ParImpar.h"
-// …
-juegos.push_back(std::make_unique<ParImpar>());
-listaJuegos.push_back("Par o Impar");
-
-
-using std::cin;
-using std::cout;
-using std::endl;
-
 
 int main() {
-    // Objeto de tipo View para enlazar con la vista
     View vistaPPAL;
     vistaPPAL.verPrincipal();
     return 0;
