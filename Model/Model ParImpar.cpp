3d63e0ece6d19275a98cdb7f2534a7fd96575bb6
SantiagoMurillas <smurillas9@icloud.com>
2025-05-06 15:41:29 -0500

    Corregido errores Parimpar.cpp

--- Model/Model ParImpar.cpp
+++ Model/Model ParImpar.cpp
@@ -1,26 +1,28 @@
 #include "ParImpar.h"
-#include <iostream>
-#include <cstdlib>
 
 void ParImpar::mostrarReglas() const {
-    std::cout << "Reglas de ‘Par o Impar’:\n"
+    std::cout << "Reglas de 'Par o Impar':\n"
               << "- Apuesta cierta cantidad de Gonzos.\n"
               << "- Elige par (0) o impar (1).\n"
-              << "- Si coincide con el tiro (0=par,1=impar), ganas tu apuesta; si no, la pierdes.\n";
+              << "- Si coincide con el tiro de la casa, ganas tu apuesta; si no, la pierdes.\n";
 }
 
-bool ParImpar::jugar(int apuesta) {
-    int tiro = std::rand() % 2;
-    int eleccion;
-    std::cout << "Elige Par(0) o Impar(1): ";
-    std::cin >> eleccion;
-    if (eleccion == tiro) {
-        std::cout << "¡Acertaste! Ganas " << apuesta << " Gonzos.\n";
-        return true;
-    }
-    std::cout << "Fallaste. Pierdes " << apuesta << " Gonzos.\n";
-    return false;
+float ParImpar::jugar(float gonzosApostar) {
+    numeroCasino = rand() % 2;
+    cout << "Elige Par(0) o Impar(1): ";
+    cin >> numeroJugador;  // 0 o 1
+    cout << "Resultado de la casa: " << numeroCasino << endl;
+    return calcularResultado(gonzosApostar);
 }
+
+float ParImpar::calcularResultado(float gonzosApostar) {
+    if (numeroJugador == numeroCasino) {
+        return gonzosApostar;
+    } else {
+        return -gonzosApostar;
+    }
+}
+
 //
 // Created by Santiago Murillas on 5/6/25.
 //
