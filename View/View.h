ae97d82cf80d2697ba2225c8f5a59608045d1d78
SantiagoMurillas <smurillas9@icloud.com>
2025-05-06 15:45:41 -0500

    .

--- View/View.h
+++ View/View.h
@@ -1,8 +1,3 @@
-//
-// Created by lufe0 on 7/05/2021.
-//  Updated: Sept 22 - 2021
-//
-
 #ifndef CASINO_VIEW_H
 #define CASINO_VIEW_H
 
@@ -10,25 +5,18 @@
 
 class View {
 private:
-    // Objeto de que se enlaza con la vista del casino
     Casino casino;
 
-    // Privado pues son de uso interno
     int mostrarMenu();
-
     void jugarView();
-
     void agregarJugador();
-
     void retirarJugador();
-
     void mostrarJugador();
-
     void recargarGonzos();
 
 public:
-    View() = default; // Escribe un constructor por defecto
+    View() = default;
     void verPrincipal();
 };
 
-#endif //CASINO_VIEW_H
+#endif // CASINO_VIEW_H
