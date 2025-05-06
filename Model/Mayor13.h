17fbbd5a7461afd2d0daf895206dfc334504ed46
SantiagoMurillas <smurillas9@icloud.com>
2025-05-06 15:27:16 -0500

    Corregido errores Juego.h

--- Model/Mayor13.h
+++ Model/Mayor13.h
@@ -1,27 +1,22 @@
-//
-// Created by lufe0 on 7/05/2021.
-//
-
 #ifndef CASINO_MAYOR13_H
 #define CASINO_MAYOR13_H
 
-
 #include "Juego.h"
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 class Mayor13 : public Juego {
-
 protected:
+    bool decidioRetirarse;
     float calcularResultado(float gonzosApostar) override;
 
-    void mostrarReglas() const;
-
 public:
-    ~Mayor13() override = default; // Los destructores deben ser sobreescritos y deben ser virtual en la clase papa. Si no tiene nada especial se puede marcar como default
-    float jugar(float gonzosApostar) override;
+    Mayor13() : decidioRetirarse(false) {}
+    ~Mayor13() override = default;
 
+    float jugar(float gonzosApostar) override;
+    void mostrarReglas() const override;
 };
 
-
-#endif //CASINO_MAYOR13_H
+#endif // CASINO_MAYOR13_H
